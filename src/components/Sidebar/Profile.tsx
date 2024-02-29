6fa72667cf079d8014c9e21e51fa13845bd2eae3
Lucas Ribeiro <lucasribeirodesouza@live.com>
2024-02-28 23:18:08 -0300

    first commit

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @next/next/no-img-element */
+import { LogOut } from 'lucide-react'
+import { Button } from '../Button'
+
+export function Profile() {
+  return (
+    <div className="grid grid-cols-profile items-center gap-3">
+      <img
+        src="https://github.com/luc-ribeiro.png"
+        className="h-10 w-10 rounded-full"
+        alt=""
+      />
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
+          Lucas Ribeiro
+        </span>
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
+          lucasribeirodesouza@live.com
+        </span>
+      </div>
+      <Button type="button" variant="ghost">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </Button>
+    </div>
+  )
+}
